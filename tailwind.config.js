4f08c53529c226f22660846def682e7eb185a1c5
Nicolas Cueca <n800912@gmail.com>
2025-12-10 17:10:44 -0500

    Initialization of the project and installation of the main libraries. Creation of first models and migrations: Habit and HabitLog

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        "./node_modules/flowbite/**/*.js" // Agregamos Flowbite
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin') // Activamos el plugin
+    ],
+};
