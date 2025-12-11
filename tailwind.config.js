e3f26fbc72212327e1c78276205f170d46e14814
Nicolas Cueca <n800912@gmail.com>
2025-12-11 10:12:36 -0500

    Design of basic user interface in client-side and configuration of tailwind config file; within this file is stablished the colors palette used for the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class', // Deshabilitar modo dark
     content: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
@@ -9,7 +10,17 @@ export default {
         "./node_modules/flowbite/**/*.js" // Agregamos Flowbite
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#00204A',
+                secondary: '#3D6A8A',
+                accent: '#CBDCEB',
+                light: '#F8F8F4',
+                last: '#999999ff',
+                button: '#00b158ff',
+                hover: '#009249ff'
+            },
+        },
     },
     plugins: [
         require('flowbite/plugin') // Activamos el plugin
